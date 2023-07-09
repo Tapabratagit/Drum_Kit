123be3b93b2f7e7cc36491767cebd508fedb0282
Tapabratagit <121946505+Tapabratagit@users.noreply.github.com>
2023-07-09 13:46:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+for(var i=0; i<document.querySelectorAll(".drum").length; i++){
+  //mouse click
+    document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+        //this.setAttribute("style","color: white;");
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        addAnimation(buttonInnerHTML);
+    });
+    //keyboard
+    document.addEventListener("keypress",function(event){
+        makeSound(event.key);
+        addAnimation(event.key);
+    });
+}
+
+function makeSound(key){
+  switch(key) {
+    case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+      break;
+    case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+      break;
+    case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+      break;
+    case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+      break;
+    case "j":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+      break;
+    case "k":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+      break;
+    case "l":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+      break;
+    default:
+      console.log(key);
+  }
+}
+
+function addAnimation(inpKey){
+  document.querySelector("."+inpKey).classList.add("pressed");
+  setTimeout(function(){
+    document.querySelector("."+inpKey).classList.remove("pressed");
+  },100);
+}
